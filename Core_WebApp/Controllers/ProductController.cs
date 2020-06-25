65e909cc9c18c811c96e705dc536753ffd473272
Mittal Rathod <rathod.mittal64@gmail.com>
2020-06-25 14:11:14 +0530

    Add project files.

---
+++ Core_WebApp/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core_WebApp.Models;
+using Core_WebApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_WebApp.Controllers
+{
+    /// <summary>
+    /// Contains action methods (methods those will be executed over HttpRequest)
+    /// ActionMethos can be either HttpGet (Default) or HttpPost(HttpPut/HttpDelete)
+    /// ActionMethod Retuens IActionResult interface
+    /// </summary>
+    public class ProductController : Controller
+    {
+        private readonly IRepository<Product, int> ProdRepository;
+        /// <summary>
+        /// Inject the ProductRepository as constructor injection
+        /// </summary>
+        public ProductController(IRepository<Product, int> ProdRepository)
+        {
+            this.ProdRepository = ProdRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var cats = await ProdRepository.GetAsync();
+            return View(cats);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Product());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            // check if the Cateogry Posted Model is valid
+            if (ModelState.IsValid)
+            {
+                // create a new Product Record
+                product = await ProdRepository.CraeteAsync(product);
+                // redirect to Index Page
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // else stey on the same page with errors
+                return View(product);
+            }
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            // search the record being edited
+            var cat = await ProdRepository.GetAsync(id);
+            // return a view with record being edited
+            return View(cat);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            // check if the Cateogry Posted Model is valid
+            if (ModelState.IsValid)
+            {
+                // update the Product Record
+                product = await ProdRepository.UpdateAsync(id, product);
+                // redirect to Index Page
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // else stey on the same page with errors
+                return View(product);
+            }
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            // search the record being edited
+            var res = await ProdRepository.DeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Core_WebApp/Services/ProductRepository.cs
@@ -0,0 +1,80 @@
+﻿using Core_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_WebApp.Services
+{
+    /// <summary>
+    /// Inject the DbContext as constructor injectation
+    /// </summary>
+    public class ProductRepository : IRepository<Product, int>
+    {
+        private readonly AppJune2020DbContext ctx;
+        public ProductRepository(AppJune2020DbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        /// <summary>
+        /// async: The method will be containing the Asynchronous call
+        /// and this method returns Task Object
+        /// await: The statement performs Async operation on separate thread
+        /// other than calling thread and the async method will returns response
+        /// to the calling thread
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public async Task<Product> CraeteAsync(Product entity)
+        {
+            // new Category will be added in DbSet
+            var result = await ctx.Products.AddAsync(entity);
+            // the transaction will be commited by adding new category in
+            // Categories table
+            await ctx.SaveChangesAsync();
+            // newly created Category will be returned
+            return result.Entity;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            // seacrh record based on Primary key
+            var cat = await ctx.Products.FindAsync();
+            if (cat != null)
+            {
+                // remove the object
+                ctx.Products.Remove(cat);
+                await ctx.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<IEnumerable<Product>> GetAsync()
+        {
+            return await ctx.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetAsync(int id)
+        {
+            return await ctx.Products.FindAsync(id);
+        }
+
+        public async Task<Product> UpdateAsync(int id, Product entity)
+        {
+            // seacrh record based on Primary key
+            var cat = await ctx.Products.FindAsync(id);
+            if (cat != null)
+            {
+                cat.ProductId = entity.ProductId;
+                cat.ProductName = entity.ProductName;
+                cat.Price = entity.Price;
+                cat.ProductRowId = entity.ProductRowId;
+                
+                await ctx.SaveChangesAsync();
+            }
+            return cat;
+        }
+    }
+}
